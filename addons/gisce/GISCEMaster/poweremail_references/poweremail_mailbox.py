4303eb7eb5e646cbcadc25f33efba18a7be9b4cd
Carles Pey <cpey@gisce.net>
2013-02-11 13:26:05 +0100

    BK treballant en el wizard

--- addons/gisce/GISCEMaster/poweremail_references/poweremail_mailbox.py
+++ addons/gisce/GISCEMaster/poweremail_references/poweremail_mailbox.py
@@ -7,16 +7,21 @@ class PoweremailMailbox(osv.osv):
     _inherit = "poweremail.mailbox"
 
     def create(self, cursor, uid, vals, context=None):
-        import pdb; pdb.set_trace()
-        return super(PoweremailMailbox,
-                     self).create(cursor, uid, values, context)
+        pe_id = super(PoweremailMailbox,
+                     self).create(cursor, uid, vals, context)
+        src_id = context.get('src_rec_id', False)
+        if src_id:
+           self.write(cursor, uid, pe_id, 
+                    {'reference': '%s,%d' % (context['src_model'], src_id)})
+        return pe_id
 
     def _get_models(self, cursor, uid, context={}):
-        cursor.execute('select m.model, m.name from ir_model order by m.model')
+        cursor.execute('select m.model, m.name from ir_model m order by m.model')
         return cursor.fetchall()
 
     _columns = {
-        'reference': fields.reference('Source Object', selection=_get_models, size=128),
+        'reference': fields.reference('Source Object', selection=_get_models,
+                                      size=128),
     }
 
 PoweremailMailbox()
