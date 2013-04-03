de35e0ba285d638f52726c86fcc892823166a7ff
Carles Pey <cpey@gisce.net>
2013-04-03 17:20:46 +0200

    BUMP to v2.27.1

--- addons/gisce/GISCEMaster/poweremail_references/__terp__.py
+++ addons/gisce/GISCEMaster/poweremail_references/__terp__.py
@@ -2,7 +2,7 @@
 {
   "name": "Poweremail References",
   "description": """Poweremail References""",
-  "version": "2.27.0",
+  "version": "2.27.1",
   "author": "GISCE",
   "category": "GISCEMaster",
   "depends": ['poweremail'],
