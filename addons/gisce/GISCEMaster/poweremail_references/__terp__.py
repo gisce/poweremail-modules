71bf5ba3d90a85389e09c3e5df2f1fb0f0e8156f
Eduard Carreras <ecarreras@gmail.com>
2014-01-31 11:34:33 +0100

    Bump to v2.37.3

--- addons/gisce/GISCEMaster/poweremail_references/__terp__.py
+++ addons/gisce/GISCEMaster/poweremail_references/__terp__.py
@@ -2,7 +2,7 @@
 {
   "name": "Poweremail References",
   "description": """Poweremail References""",
-  "version": "2.37.2",
+  "version": "2.37.3",
   "author": "GISCE",
   "category": "GISCEMaster",
   "depends": ['poweremail'],
