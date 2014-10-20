05064de551fc5973fc2529a528870fecd7d675c3
Eduard Carreras <ecarreras@gmail.com>
2014-10-20 15:53:29 +0200

    Bump to v0.2.4

--- poweremail_oorq/__terp__.py
+++ poweremail_oorq/__terp__.py
@@ -2,7 +2,7 @@
 {
   "name": "Poweremail OORQ",
   "description": """Poweremail using OORQ""",
-  "version": "0.2.3",
+  "version": "0.2.4",
   "author": "GISCE",
   "category": "GISCEMaster",
   "depends": ['base', 'poweremail', 'oorq'],
