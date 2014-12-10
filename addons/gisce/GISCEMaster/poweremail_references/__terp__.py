ea34a683274e73d226b91c839f2f8a6517607a97
Guillem Julià Blasi <gjulia@gisce.net>
2014-12-10 12:13:18 +0100

    Bump to v2.47.6

--- addons/gisce/GISCEMaster/poweremail_references/__terp__.py
+++ addons/gisce/GISCEMaster/poweremail_references/__terp__.py
@@ -2,7 +2,7 @@
 {
   "name": "Poweremail References",
   "description": """Poweremail References""",
-  "version": "2.47.5",
+  "version": "2.47.6",
   "author": "GISCE",
   "category": "GISCEMaster",
   "depends": ['poweremail'],
