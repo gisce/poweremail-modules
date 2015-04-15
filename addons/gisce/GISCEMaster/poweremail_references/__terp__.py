10ce8571e7f7b7cec9b9efaa5b015c76735bd6d9
Agustí Fita i Borrell <afita@gisce.net>
2015-04-15 10:31:53 +0200

    Bump to v2.51.4

--- addons/gisce/GISCEMaster/poweremail_references/__terp__.py
+++ addons/gisce/GISCEMaster/poweremail_references/__terp__.py
@@ -2,7 +2,7 @@
 {
   "name": "Poweremail References",
   "description": """Poweremail References""",
-  "version": "2.51.3",
+  "version": "2.51.4",
   "author": "GISCE",
   "category": "GISCEMaster",
   "depends": ['poweremail'],
