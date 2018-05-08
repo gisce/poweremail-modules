70eaa73735a16ff4dded3d95008f7a61e13c1aa5
Jaume Florez <jflorez@gisce.net>
2018-05-08 17:04:40 +0200

    Correctly indent terp file

--- poweremail_oorq/__terp__.py
+++ poweremail_oorq/__terp__.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 {
-  "name": "Poweremail OORQ",
-  "description": """Poweremail using OORQ""",
-  "version": "0.2.4",
-  "author": "GISCE",
-  "category": "GISCEMaster",
-  "depends": ['base', 'poweremail', 'oorq'],
-  "init_xml": [],
-  "demo_xml": [],
-  "update_xml": [],
-  "active": False,
-  "installable": True
+    "name": "Poweremail OORQ",
+    "description": """Poweremail using OORQ""",
+    "version": "0.2.4",
+    "author": "GISCE",
+    "category": "GISCEMaster",
+    "depends": ['base', 'poweremail', 'oorq'],
+    "init_xml": [],
+    "demo_xml": [],
+    "update_xml": [],
+    "active": False,
+    "installable": True
 }
