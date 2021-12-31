c80710517a639022de7a7d54dd14bb8d9bbe6d03
Eduard Carrerars <ecarreras@gisce.net>
2021-12-31 10:04:51 +0100

    Add generate_mail in background

--- poweremail_oorq/__init__.py
+++ poweremail_oorq/__init__.py
@@ -1,2 +1,4 @@
-import poweremail_mailbox
-import poweremail_send_wizard
+from __future__ import absolute_import
+from . import poweremail_mailbox
+from . import poweremail_send_wizard
+from . import poweremail_template
