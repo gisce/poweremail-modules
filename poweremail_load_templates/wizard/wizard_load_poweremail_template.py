d0e9403fc297228b5b09fb5ddfb802eaaae67881
Luis Carlos Bautista <lcbautista@gisce.net>
2023-07-19 16:38:45 +0200

    fix: bad id. imp: readability

--- poweremail_load_templates/wizard/wizard_load_poweremail_template.py
+++ poweremail_load_templates/wizard/wizard_load_poweremail_template.py
@@ -11,43 +11,16 @@ class WizardLoadPoweremailTemplate(osv.osv_memory):
         if context is None:
             context = {}
         wiz = self.browse(cursor, uid, ids[0], context=context)
-        poweremail_template_obj = self.pool.get('poweremail.templates')
-        poweremail_template_source_obj = self.pool.get(
-            'poweremail.templates.source'
-        )
-        res_lang_obj = self.pool.get('res.lang')
-        # Read source ids
-        source_v = poweremail_template_obj.read(
-            cursor, uid, ids[0], ['source_ids'], context=context
-        )
-        source_ids = source_v['source_ids']
-        for source_id in source_ids:
-            template_source_v = poweremail_template_source_obj.read(
-                cursor, uid, source_id, [
-                    'template_id', 'source', 'lang'
-                ],
-                context=context
-            )
+        for source in wiz.template_id.source_ids:
             file_path = "{}/{}".format(
-                config['addons_path'], template_source_v['source']
+                config['addons_path'], source.source
             )
             if os.path.exists(file_path):
                 with open(file_path, "r") as mf:
                     ctx = context.copy()
-                    res_lang_v = res_lang_obj.read(
-                        cursor, uid, template_source_v['lang'][0], [
-                            'code'
-                        ],
-                        context=context
-                    )
-                    ctx['lang'] = res_lang_v['code']
+                    ctx['lang'] = source.lang.code
                     file_contents = mf.read()
-                    poweremail_template_obj.write(
-                        cursor, uid, template_source_v['template_id'][0], {
-                            'def_body_text': file_contents
-                        },
-                        context=ctx
-                    )
+                    wiz.template_id.write({'def_body_text': file_contents}, context=ctx)
             else:
                 path_split = file_path.split('/')
                 raise osv.except_osv(
