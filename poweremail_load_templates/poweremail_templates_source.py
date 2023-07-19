317e9054a6b7e3c5818b72d6904171cdb8e78251
Luis Ka <lcbautista@gisce.net>
2023-07-19 16:16:38 +0200

    Increase source character limit

--- poweremail_load_templates/poweremail_templates_source.py
+++ poweremail_load_templates/poweremail_templates_source.py
@@ -10,7 +10,7 @@ class PoweremailTemplatesSource(osv.osv):
         'template_id': fields.many2one(
             'poweremail.templates', 'Template', select=1, required=True
         ),
-        'source': fields.char('Source', size=64, required=True),
+        'source': fields.char('Source', size=256, required=True),
         'lang': fields.many2one(
             'res.lang', 'Language', select=1, required=True
         ),
