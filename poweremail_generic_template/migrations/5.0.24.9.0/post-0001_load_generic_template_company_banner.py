f83756f66771802770fe7159020ad10cea78ac16
Luis Ka <lcbautista@gisce.net>
2024-08-02 13:29:06 +0200

    imp: avoid redundant update

--- poweremail_generic_template/migrations/5.0.24.9.0/post-0001_load_generic_template_company_banner.py
+++ poweremail_generic_template/migrations/5.0.24.9.0/post-0001_load_generic_template_company_banner.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from oopgrade.oopgrade import load_data
+from tools import config
 
 
 def up(cursor, installed_version):
-    if not installed_version:
+    if not installed_version or config.updating_all:
         return
     load_data(cursor, 'poweremail_generic_template', 'data/banners/banner_generic_email_template_company.xml')
 
