f1c025a5b5181c275ab596bec41f5356a59195e7
mcalvo <mcalvo@gisce.net>
2025-08-20 12:12:49 +0200

    chore: increase powersms templates name size

---
+++ powersms/migrations/5.0.25.9.0/pre-0001_increase_name_size.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import logging
+import pooler
+from oopgrade.oopgrade import add_columns, column_exists
+
+
+def up(cursor, installed_version):
+    if not installed_version:
+        return
+
+    logger = logging.getLogger('openerp.migration')
+
+    logger.info('Increasing size of powersms.templates name from 100 to 200')
+    cursor.execute('ALTER TABLE powersms.templates ALTER COLUMN name type varchar(200)')
+    logger.info('Migration successfully completed')
+
+
+
+def down(cursor, installed_version):
+    pass
+
+
+migrate = up
