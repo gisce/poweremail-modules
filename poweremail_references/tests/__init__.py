c46f6a127f7e13898a2e1969966ca09de836e5a4
Luis Ka <lcbautista@gisce.net>
2026-06-29 08:50:48 +0200

    fix: ni idea que ha fet el bot aqui

--- poweremail_references/tests/__init__.py
+++ poweremail_references/tests/__init__.py
@@ -3,5 +3,3 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 
 from .test_poweremail_references import TestPoweremailReferences
-
-__all__ = ['TestPoweremailReferences']
